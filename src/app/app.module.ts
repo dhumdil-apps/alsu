c46b34daed5dbe89c1aadf4fb3d2fa77b94abde5
Martin Lakatos <m16peter@yahoo.ro>
2017-01-11 13:43:58 +0100

    [*] APP FILE-STRUCTURE
    alsu
    ----code-blocks
        ----blocks
    	----types
    ----left-side
        ----input
    ----right-side
        ----output
    ----toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,21 @@ import { NgModule }         from '@angular/core';
 import { FormsModule }      from '@angular/forms';
 import { HttpModule }       from '@angular/http';
 
-// app
 import { AppComponent }     from './app.component';
 
-// structure
-import { MainComponent }        from '../alsu/structure/main.component';
-import { ToolbarComponent }     from '../alsu/structure/toolbar/toolbar.component';
-import { RightSideComponent }    from '../alsu/structure/right-side/right-side.component';
-import { LeftSideComponent }   from '../alsu/structure/left-side/left-side.component';
-
-// // alsu
+import { MainComponent }        from '../alsu/main.component';
+import { ToolbarComponent }     from '../alsu/toolbar/toolbar.component';
+import { LeftSideComponent }   from '../alsu/left-side/left-side.component';
+// import { InputComponent }       from '../alsu/left-side/input/input.component';
 // import { CodeBlocksComponent }  from '../alsu/main/code-blocks/code-blocks.component';
 // import { BlockComponent }       from '../alsu/main/code-blocks/block/block.component';
-// import { InputComponent }       from '../alsu/left-side/input/input.component';
+import { RightSideComponent }    from '../alsu/right-side/right-side.component';
 // import { OutputComponent }      from '../alsu/right-side/output/output.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        // structure
+
         MainComponent,
         ToolbarComponent,
         LeftSideComponent,
